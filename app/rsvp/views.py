2a0f7dd64189a2f64ecf8ce85ade4888a34b9297
ketsun <ketsuniku@gmail.com>
2016-05-13 15:51:59 -0700

    Added a search filter backend to allow case insensitive searches by an RSVP invitee name

--- app/rsvp/views.py
+++ app/rsvp/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_condition import Or
+from rest_framework import filters
 from common.permissions import ApiTokenPermissions, GetPermission
 from .models import Invitation, Invitee
 from .serializers import InvitationSerializer, InviteeSerializer
@@ -8,6 +9,8 @@ from .serializers import InvitationSerializer, InviteeSerializer
 class InvitationViewSet(viewsets.ModelViewSet):
     serializer_class = InvitationSerializer
     permission_classes = (ApiTokenPermissions, )
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('invitees__name', )
 
     def get_queryset (self):
         return Invitation.objects.all()
