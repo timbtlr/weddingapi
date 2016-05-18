d687b047e20d4d0473832c35b1502db493e47fc2
ketsun <ketsuniku@gmail.com>
2016-05-17 18:50:56 -0700

    Added song request field to invitation

--- app/rsvp/models.py
+++ app/rsvp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Invitation(models.Model):
     invite_name = models.CharField(max_length=20, null=True)
     message = models.CharField(max_length=1000, null=True, blank=True)
+    song_request = models.CharField(max_length=1000, null=True, blank=True)
 
     def __unicode__(self):
         return self.invite_name
